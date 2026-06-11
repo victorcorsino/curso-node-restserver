eda546c0568772b70e7e5c4be43138ad460c280c
victorcorsino <vcorsinoa@gmail.com>
2026-06-11 11:29:51 -0400

    Fin seccion 10 version 3.0.0

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -71,9 +71,6 @@ const usuariosDelete = async(req, res = response) => {
 
     const { id } = req.params;
 
-    //Fisicamente lo borramos
-    //const usuario = await Usuario.findByIdAndDelete(id);
-
     const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
 
     res.json(usuario);
---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,55 @@
+const { request, response } = require('express');
+const jwt = require('jsonwebtoken');
+
+const Usuario = require('../models/usuario');
+
+const validarJWT = async(req = request, res = response, next) => {
+
+    const token = req.header('x-token');
+
+    if ( !token ) {
+        return res.status(401).json({
+            msg: 'No hay token en la petición'
+        });
+    }
+
+    try {
+
+        const { uid } = jwt.verify( token, process.env.SECRETORPRIVATEKEY );
+
+        // leer el usuario que corresponde al uid
+        const usuario = await Usuario.findById(uid);
+
+        if ( !usuario ) {
+            return res.status(401).json({
+                msg: 'Token no válido - usuario no existe en DB'
+            });
+        }
+
+        // Verificar si el uid tiene estado true
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Token no válido - usuario con estado: false'
+            });
+        }
+
+        req.usuario = usuario;
+
+        next(); 
+
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            msg: 'Token no válido'
+        });
+    }
+
+    //console.log(token);
+
+    
+
+}
+
+module.exports = {
+    validarJWT
+}
